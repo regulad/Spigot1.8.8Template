89843274f04a160debfa7bf3361d2064e063cf5c
Parker Wahle <parkeredwardwahle2017@gmail.com>
2021-12-27 11:50:59 -0500

    Obfuscation & etc

--- src/main/java/xyz/regulad/spigottemplate/Spigot188Template.java
+++ src/main/java/xyz/regulad/spigottemplate/Spigot188Template.java
@@ -1,24 +1,42 @@
 package xyz.regulad.spigottemplate;
 
 import lombok.Getter;
+import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.Nullable;
+import xyz.regulad.spigottemplate.api.API;
 
-public class Spigot188Template extends JavaPlugin {
+public class Spigot188Template extends JavaPlugin implements API {
     @Getter
     private static @Nullable Spigot188Template instance;
+    @Getter
+    private @Nullable Metrics metrics;
+    @Getter
+    private @Nullable BukkitAudiences bukkitAudiences;
 
     @Override
     public void onEnable() {
         // Setup instance access
         instance = this;
+        // Setup config
+        this.saveDefaultConfig();
+        // Setup adventure
+        this.bukkitAudiences = BukkitAudiences.create(this);
         // Setup bStats metrics
-        new Metrics(this, 13741); // TODO: Replace this in your plugin!
+        this.metrics = new Metrics(this, 13741); // TODO: Replace this in your plugin!
     }
 
     @Override
     public void onDisable() {
+        // Discard instance access
         instance = null;
+        // Discard adventure
+        if (this.bukkitAudiences != null) {
+            this.bukkitAudiences.close();
+            this.bukkitAudiences = null;
+        }
+        // Discard bStats metrics
+        this.metrics = null;
     }
 }
---
+++ src/main/java/xyz/regulad/spigottemplate/api/API.java
@@ -0,0 +1,6 @@
+package xyz.regulad.spigottemplate.api;
+
+import org.bukkit.plugin.Plugin;
+
+public interface API extends Plugin {
+}
